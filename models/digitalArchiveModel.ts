c9f70eaa7b3c504205a7918888653a064630c7a6
Shine Panjwani <shinepanjwani18@gmail.com>
2025-11-26 17:14:45 +0530

    Update archieve model and digitalArchieve.json

--- models/digitalArchiveModel.ts
+++ models/digitalArchiveModel.ts
@@ -2,22 +2,19 @@ import mongoose, { Schema, Document } from "mongoose";
 
 export interface IDigitalArchive extends Document {
   monasteryId: mongoose.Types.ObjectId;
-  type: string;
+  title : string,
   fileUrl: string;
   description: string;
-  category: string;
   language: string;
   images: string[];
 }
-
 const DigitalArchiveSchema = new Schema<IDigitalArchive>(
   {
     monasteryId: { type: Schema.Types.ObjectId, ref: "Monastery" },
-    type: String,
+    title : String,
     fileUrl: String,
     images: [String],
     description: String,
-    category: String,
     language: String,
   },
   { timestamps: true }
